de7475a013a70eb73397d32fd86ac57e428bce50
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-10 07:08:22 +0200

    Created a formDATA object to handle data

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
 });
